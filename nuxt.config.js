3d3e0e246cf004f90f80325faffbb93479fb3163
devezy <s@devezy.com>
2022-08-24 18:55:21 +0500

    address

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,8 +73,8 @@ export default {
   axios: {
     proxy: true,
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3000'
-    // baseURL: 'https://v3ui.avokado.sa'
+    // baseURL: 'http://localhost:3000'
+    baseURL: 'https://v3ui.avokado.sa'
   },
   proxy: {
     '/v3': {target: 'https://avokado.sa'},
