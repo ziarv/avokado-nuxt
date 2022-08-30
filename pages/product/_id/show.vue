e62dc95f0e4120b738721a643c5f0fb0e5d93599
devezy <s@devezy.com>
2022-08-30 18:20:57 +0500

    fix

--- pages/product/_id/show.vue
+++ pages/product/_id/show.vue
@@ -1,69 +1,94 @@
 <template>
   <div v-if="product.id">
-    <section>
+    <section class="mb-5">
       <div
-        class="mx-20 flex mt-20 xs:!mt-4 sm:!mt-5 xs:!mx-5 sm:!mx-5 flex-row flex-wrap justify-between xs:!mb-10 sm:!mb-10">
+        class="mx-20 flex mt-20 xs:!mt-10 sm:!mt-5 xs:!mx-10 sm:!mx-5 flex-row flex-wrap justify-between xs:!mb-10 sm:!mb-10">
         <div class="product_slider w-[49%] xs:!w-[100%] sm:!w-[100%] h-[100%] relative">
           <div ref="swiper" class="swiper mySwiper_12 mySwiper_5">
             <div class="swiper-wrapper">
               <div class="cards product_slider_1 swiper-slide justify-center">
-                <div class="cardse_sec_main">
-                  <div class="cardse_first">
-                    <!--                    <img src="@/assets/img/Back_green.svg" alt="">-->
-                  </div>
-                  <div class="cardse_sec">
-                    <!--                    <img src="@/assets/img/share_green.svg" alt="">-->
-                    <!--                    <img src="@/assets/img/dil_green.svg" alt="">-->
-                  </div>
-                </div>
-                <div class="cardse">
-                  <img src="@/assets/img/apple.png" alt="">
-                </div>
-              </div>
-              <div class="cards product_slider_1 swiper-slide justify-center">
-                <div class="cardse_sec_main">
-                  <div class="cardse_first">
-                  </div>
-                  <div class="cardse_sec">
-                  </div>
-                </div>
                 <div class="cardse">
                   <img :src="product.images.thumbnail" alt="thumbnail">
                 </div>
               </div>
-
             </div>
             <div class="swiper-button-next"><i class="fa-solid fa-arrow-right"></i></div>
             <div class="swiper-button-prev"><i class="fa-solid fa-arrow-left"></i></div>
           </div>
+          <!--          <div class="swiper mySwiper_4">-->
+          <!--            <div class="swiper-wrapper">-->
+          <!--              <div class="swiper-slide">-->
+          <!--                <img src="@/assets/img/apple.png" alt="image"/>-->
+          <!--              </div>-->
+          <!--              <div class="swiper-slide">-->
+          <!--                <img src="@/assets/img/apple.png" alt="image"/>-->
+          <!--              </div>-->
+          <!--              <div class="swiper-slide">-->
+          <!--                <img src="@/assets/img/apple.png" alt="image"/>-->
+          <!--              </div>-->
+          <!--              <div class="swiper-slide">-->
+          <!--                <img src="@/assets/img/apple.png" alt="image"/>-->
+          <!--              </div>-->
+          <!--              <div class="swiper-slide">-->
+          <!--                <img src="@/assets/img/apple.png" alt="image"/>-->
+          <!--              </div>-->
+          <!--              <div class="swiper-slide">-->
+          <!--                <img src="@/assets/img/apple.png" alt="image"/>-->
+          <!--              </div>-->
+          <!--              <div class="swiper-slide">-->
+          <!--                <img src="@/assets/img/apple.png" alt="image"/>-->
+          <!--              </div>-->
+          <!--              <div class="swiper-slide">-->
+          <!--                <img src="@/assets/img/apple.png" alt="image"/>-->
+          <!--              </div>-->
+          <!--              <div class="swiper-slide">-->
+          <!--                <img src="@/assets/img/apple.png" alt="image"/>-->
+          <!--              </div>-->
+          <!--              <div class="swiper-slide">-->
+          <!--                <img src="@/assets/img/apple.png" alt="image"/>-->
+          <!--              </div>-->
+          <!--            </div>-->
+          <!--          </div>-->
         </div>
         <div
           class="product_slider_09 px-[12px] py-12 xs:!py-4 sm:!py-4 w-[48%] xs:!w-[100%] sm:!w-[100%] rounded-[20px] bg-[#FFFFFF]">
           <div
             class="product_slider_09_heading flex flex-row flex-wrap justify-between px-[20px] xs:!px-[0px] sm:!px-[0px]">
-            <h1 class="text-[32px] xs:!text-[24px] sm:!text-[26px] xs:!mb-2.5 sm:!mb-2.5 font-['futur-extra'] mb-5">
+            <h1
+              class="text-[32px] xs:!text-[24px] sm:!text-[26px] xs:!mb-2.5 sm:!mb-2.5 font-['futur-extra'] mb-3">
               {{ product.name }}
               <small class="!text-[18px]"> {{ product.avokadoAttributes.avokado_item_unit }}</small>
             </h1>
             <div class="product_slider_09_heading_icon">
-              <!--              <img src="@/assets/img/heart_dil.svg" alt="">-->
-              <!--              <img src="@/assets/img/share.svg" alt="">-->
+
+<!--              <img src="@/assets/img/Vector.svg" alt="">-->
+<!--              <img src="@/assets/img/share.svg" alt="">-->
             </div>
+
             <h5 class="w-[100%] text-[22px] xs:!text-[14px] sm:!text-[16px] text-[#7CB118] font-bold mb-5">
               {{ product.price_tax_inc }}
               {{ product.currency_symbol.toUpperCase() }}
             </h5>
+            <!--            <span class="w-[100%] text-[14px] xs:!text-[12px] sm:!text-[12px] flex text-[#B7B7B7] mb-5"><img-->
+            <!--              src="@/assets/img/Star.svg" alt="">-->
+            <!--                        <span class="mx-1 text-[#000000]">4.9</span>(200)-->
+            <!--                    </span>-->
             <p
-              v-if="product.availableQuantity > 0"
-              class="w-[100%] flex items-center xs:!text-[10px] sm:!text-[10px] mb-5"><img
+v-if="product.availableQuantity > 0"
+               class="w-[100%] flex items-center xs:!text-[10px] sm:!text-[10px] mb-5"><img
               src="@/assets/img/circle.svg"
               class="mr-4 xs:!w-[10px] xs:!mr-[0.5rem] sm:!mr-[0.5rem] sm:!w-[10px]" alt="">
-              In Stock
+              In
+              Stock
             </p>
+            <!--            <button-->
+            <!--              class="bg-[#7CB118] text-[#FFFFFF] py-2 rounded-full px-9 mb-12 xs:!mx-5 sm:!mx-5 xs:!my-1 sm:!my-1 xs:!mb-3 sm:!mb-3">-->
+            <!--              Add-->
+            <!--              to cart-->
+            <!--            </button>-->
             <div class="product_btn xs:!w-[23%]">
               <button
-                :disabled="qty <= 0" class="add_product addShow_product xs:!text-[12px] xs:!py-[0px] xs:!px-[5px]"
-                @click="remove">
+:disabled="qty <= 0" class="add_product addShow_product xs:!text-[12px] xs:!py-[0px] xs:!px-[5px]" @click="remove">
                 <i class="fa-solid fa-minus"></i></button>
               <input v-model="qty" type="text" class="add_product addShow_product xs:!w-[25px]">
               <button class="add_product addShow_product xs:!text-[12px] xs:!py-[0px] xs:!px-[5px]" @click="add">
@@ -79,60 +104,27 @@
             </p>
           </div>
         </div>
-        <div class="show">
-          <h1 class="text-[32px] xs:!text-[24px] sm:!text-[26px] xs:!mb-2.5 sm:!mb-2.5 font-['futur-extra'] mb-5">
-            {{ product.name }}
-          </h1>
-          <div class="product_slider_09_heading_icon">
-
-          </div>
-          <h5 class="w-[100%] text-[22px] xs:!text-[14px] sm:!text-[16px] text-[#7CB118] font-bold mb-5">
-            {{ product.price_tax_inc }}
-            {{ product.currency_symbol.toUpperCase() }}
-            <span>/{{ product.avokadoAttributes.avokado_item_unit }}</span>
-          </h5>
-          <span class="w-[100%] text-[14px] xs:!text-[12px] sm:!text-[12px] flex text-[#B7B7B7] mb-5">
-<!--            <img-->
-            <!--            src="@/assets/img/Star.svg" alt="">-->
-            <!--                    <span class="mx-1 text-[#000000]">4.9</span>(200)-->
-                </span>
-          <p
-v-if="product.availableQuantity > 0"
-             class="w-[100%] flex items-center xs:!text-[10px] sm:!text-[10px] mb-5"><img
-            src="@/assets/img/circle.svg"
-            class="mr-4 xs:!w-[10px] xs:!mr-[0.5rem] sm:!mr-[0.5rem] sm:!w-[10px]" alt="">
-            In
-            Stock</p>
-          <div class="product_btn xs:!w-[23%]">
-            <button
-              :disabled="qty <= 0" class="add_product addShow_product xs:!text-[12px] xs:!py-[0px] xs:!px-[5px]"
-              @click="remove">
-              <i class="fa-solid fa-minus"></i></button>
-            <input v-model="qty" type="text" class="add_product addShow_product xs:!w-[25px]">
-            <button class="add_product addShow_product xs:!text-[12px] xs:!py-[0px] xs:!px-[5px]" @click="add">
-              <i class="fa-solid fa-plus"></i>
-            </button>
-          </div>
-        </div>
-        <!-- <hr class="w-[100%] mb-7 xs:!mb-3 sm:!mb-3"> -->
-        <div>
-          <h4
-            class="w-[100%] text-[22px] xs:!text-[14px] sm:!text-[14px] text-[#000000] font-bold mb-5 situation">
-            Overview
-          </h4>
-        </div>
-
-        <div
-          class="product_slider_boot situation_heandle px-[12px] py-12 xs:!py-4 sm:!py-4 w-[48%] xs:!w-[100%] sm:!w-[100%] rounded-[20px] bg-[#FFFFFF]">
-          <div
-            class="product_slider_09_heading flex flex-row flex-wrap justify-between px-[20px] xs:!px-[0px] sm:!px-[0px]">
-            <p class="w-[100%] text-[#838383] text-[16px] xs:!text-[12px] sm:!text-[12px]">
-              {{ product.description }}
-            </p>
-          </div>
-        </div>
       </div>
     </section>
+
+    <!--    <section>-->
+    <!--      <div class="mx-20 xs:!mx-10 sm:!mx-5 flex">-->
+    <!--        <div class="desci">-->
+    <!--          <h1 class="text-[26px] font-['futur-extra'] mb-10 xs:!text-[22px] sm:!text-[22px] xs:!mb-5 sm:!mb-5">-->
+    <!--            Description</h1>-->
+    <!--          <p class="text-[25px] text-[#838383] xs:!text-[15px] sm:!text-[15px]">Lorem ipsum dolor sit amet,-->
+    <!--            consectetur adipiscing elit. Aliquam-->
+    <!--            auctor a tellus et sodales.-->
+    <!--            Pellentesque vel hendrerit dui. Curabitur molestie lacus eu eros placerat fermentum. Maecenas semper-->
+    <!--            nunc turpis, a sagittis urna mattis eget. </p>-->
+    <!--          <hr class="mt-10">-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--    </section>-->
+
+    <!--    <product-section></product-section>-->
+    <hr class="m-20 xs:!mt-10 xs:!mb-5 xs:!mx-10 sm:!mt-10 sm:!mb-5 sm:!mx-10">
+    <key-values></key-values>
   </div>
 </template>
 
@@ -140,8 +132,9 @@ v-if="product.availableQuantity > 0"
 import {mapActions} from "vuex";
 import Swiper from 'swiper/swiper-bundle.min';
 import 'swiper/swiper-bundle.min.css';
+
 export default {
-  name: "ShowProduct",
+  name: "ProductIndex",
   data() {
     return {
       qty: 0,
