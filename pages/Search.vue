8739dc4d04f7ba3042c52fade77ffe21306e71b9
devezy <s@devezy.com>
2022-08-31 19:11:10 +0500

    fix

--- pages/Search.vue
+++ pages/Search.vue
@@ -1,10 +1,16 @@
 <template>
-  <section class="flex px-10 ">
-    <div class="flex flex-row flex-wrap justify-between xs:flex-col sm:flex-col">
-      <div class="heading flex flex-row flex-wrap justify-between mx-4 ">
-        <h1> {{ $t('pages.search.search_product') }} </h1>
+  <section>
+    <div class="m-auto  flex-row flex-wrap flex my-14 justify-center">
+      <div class="main_content w-[76%] sm:!w-[100%] sm:mt-[50px]">
+        <div class="flex flex-row flex-wrap justify-between">
+          <div class="heading flex flex-row flex-wrap justify-between mx-4">
+            <h1> {{ $t('pages.search.search_product') }} </h1>
+          </div>
+          <product-list
+v-for="(product,index) in products" :key="index" class="mb-5" ca
+                        :product="product"></product-list>
+        </div>
       </div>
-      <product-list v-for="(product,index) in products" :key="index" class="mb-5" ca :product="product"></product-list>
     </div>
   </section>
 </template>
