b09feae80c467ce5c45b48fe54ea68de877cf6a9
devezy <s@devezy.com>
2022-09-08 17:42:23 +0500

    fix

---
+++ pages/category/_id/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowProduct",
+  data() {
+    return {
+      qty: 0
+    }
+  },
+  mounted() {
+    this.$router.push({
+      path: this.localePath(`/category/${this.$route.params.id}/slug?cid=${this.$route.params.id}`)
+    })
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/product/_id/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowProduct",
+  data() {
+    return {
+      qty: 0
+    }
+  },
+  mounted() {
+    this.$router.push({
+      path: this.localePath(`/product/${this.$route.params.id}/slug?pid=${this.$route.params.id}`)
+    })
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
